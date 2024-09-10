e4e168ded16dfc52e6762466440ba6df0de7c4d7
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-10 22:36:34 +0200

    Add validation constraints and messages for ShippingDetailsDTO
    - Added @NotBlank and @PositiveOrZero annotations to ShippingDetailsDTO fields.
    - Updated messages.properties with validation messages for ShippingDetailsDTO.

--- src/main/java/com/example/shoppingcartservice/adapter/in/web/dto/ShippingDetailsDTO.java
+++ src/main/java/com/example/shoppingcartservice/adapter/in/web/dto/ShippingDetailsDTO.java
@@ -1,17 +1,26 @@
 package com.example.shoppingcartservice.adapter.in.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ShippingDetailsDTO {
+
+    @NotBlank(message = "{shipping.address.notblank}")
     private String shippingAddress;
+
+    @NotBlank(message = "{shipping.method.notblank}")
     private String shippingMethod;
     private Date estimatedDeliveryDate;
-    private double shippingCost;
+
+    @PositiveOrZero(message = "{shipping.cost.positiveorzero}")
+    private BigDecimal shippingCost;
 }
