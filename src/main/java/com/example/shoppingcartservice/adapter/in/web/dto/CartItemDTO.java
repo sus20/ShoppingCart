00cdc5edffa797c33dcfa44ba586609aa7b0b1ff
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-10 22:25:05 +0200

    Refactor CartItemDTO with validation and BigDecimal
    - Added @NotBlank, @Positive, and @PositiveOrZero annotations to fields.
    - Changed unitPrice and totalPrice to BigDecimal for precision.
    - Updated validation messages in CartItemDTO.

--- src/main/java/com/example/shoppingcartservice/adapter/in/web/dto/CartItemDTO.java
+++ src/main/java/com/example/shoppingcartservice/adapter/in/web/dto/CartItemDTO.java
@@ -1,20 +1,40 @@
 package com.example.shoppingcartservice.adapter.in.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartItemDTO {
+
+    @NotBlank(message = "{product.id.notblank}")
     private String productId;
+
+    @NotBlank(message = "{product.name.notblank}")
     private String productName;
+
+    @NotBlank(message = "{product.image.notblank}")
     private String productImage;
+
+    @NotBlank(message = "{product.description.notblank}")
     private String productDescription;
+
+    @PositiveOrZero(message = "{quantity.positiveorzero}")
     private int quantity;
-    private double unitPrice;
-    private double totalPrice; // quantity * unitPrice
+
+    @Positive(message = "{unit.price.positive}")
+    private BigDecimal unitPrice;
+
+    @PositiveOrZero(message = "{total.price.positiveorzero}")
+    private BigDecimal totalPrice; // quantity * unitPrice
+
     private boolean inStock;
     private boolean backOrder;
 }
