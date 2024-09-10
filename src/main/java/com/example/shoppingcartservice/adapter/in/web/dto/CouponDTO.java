2e82a13b420d07e18aa4c463628e999d1dc063bf
Sapkota Sushma <susma.sapkota5@gmail.com>
2024-09-10 22:32:24 +0200

    Add validation constraints and messages for CouponDTO
    - Added @NotBlank and @Positive annotations to CouponDTO fields.
    - Updated messages.properties with validation messages for CouponDTO.

--- src/main/java/com/example/shoppingcartservice/adapter/in/web/dto/CouponDTO.java
+++ src/main/java/com/example/shoppingcartservice/adapter/in/web/dto/CouponDTO.java
@@ -1,14 +1,24 @@
 package com.example.shoppingcartservice.adapter.in.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CouponDTO {
+
+    @NotBlank(message = "{coupon.code.notblank}")
     private String couponCode;
-    private double discountAmount;
+
+    @Positive(message = "{discount.amount.positive}")
+    private BigDecimal discountAmount;
+
+    @NotBlank(message = "{discount.type.notblank}")
     private String discountType; // Percentage or Flat-rate
 }
